facd7511f628cf177d829b6f684f1591ac3d491e
Mehdi Sheykhveysi <m.sheykhveysi4680@gmail.com>
2021-02-06 22:29:10 +0330

    Add project files.

---
+++ BibTexNetStandardLiberary/Token.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+namespace BibTeXLibrary
+{
+    internal struct Token
+    {
+        public TokenType Type;
+        public string Value;
+
+        public Token(TokenType type, string value = "")
+        {
+            Type = type;
+            Value = value;
+            Debug.WriteLine(type + "\t" + value);
+        }
+    }
+
+    public enum TokenType
+    {
+        Start,
+        //oy2fsmhbm5gtky64zgnqypf3qctjoj2uxoiitsaubxgcum
+        Name,
+        String,
+
+        Quotation,
+
+        LeftBrace,
+        RightBrace,
+
+        Equal,
+        Comma,
+
+        Concatenation,
+
+        EOF
+    }
+}
